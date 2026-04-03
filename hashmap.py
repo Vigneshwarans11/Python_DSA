2e354cb66ec6f86cc78bc3ff91206e3c6d85f18f
Vigneshwarans11 <vigneshsrinivasan1111@gmail.com>
2026-04-03 22:23:17 +0530

    first commit

---
+++ hashmap.py
@@ -0,0 +1,8 @@
+items=["pen", "book", "pen", "pencil", "book", "pen"]
+count={}
+for products in items:
+    if products in count:
+        count[products]+=1
+    else:
+        count[products]=1
+print(count)
